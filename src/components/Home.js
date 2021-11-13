7415b07acdb2c6ea40ac6bf0661c7291c6fedfdf
Dustin Ward <dustin.ward@uleth.ca>
2021-11-13 13:08:49 -0700

    Calendar added to home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,19 @@
-import React from "react";
+import React, {useState} from "react";
+import Calendar from "react-calendar";
+import 'react-calendar/dist/Calendar.css';
 
 function Home() {
   const token = localStorage.getItem('token'); //check if set to null
+  const [calValue, setCalValue] = useState(new Date());
+
   return (
     <div className="Home">
       <div class="container">
         <div class="row align-items-center my-5">
           <div class="col-lg-7">
+            {token != null && 
+              <Calendar onChange={setCalValue} value={calValue}/>
+            }
           </div>
           <div class="col-lg-5">
             <h1 >Home </h1> {token}
